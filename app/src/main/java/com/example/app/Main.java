c11ed7468ab042ce5e303f0fa3200149fc50ee2a
임수영 <sylim@etoos.com>
2023-01-26 17:59:17 +0900

    [ADD] 프로젝트 init

---
+++ app/src/main/java/com/example/app/Main.java
@@ -0,0 +1,11 @@
+package com.example.app;
+
+
+public class Main {
+    public static void main(String[] args) {
+        SampleGetter sampleGetter = new SampleGetter();
+        sampleGetter.getName();
+        sampleGetter.getAge();
+    }
+
+}
---
+++ app/src/main/java/com/example/app/Sample.java
@@ -0,0 +1,9 @@
+package com.example.app;
+
+import com.example.annotation.CustomGetter;
+
+@CustomGetter
+public class Sample {
+    private String name;
+    private Integer age;
+}
